b2e619ba0a71e89778df2623ceb3fb160256f023
Bappi Saha <bappi35-2097@diu.edu.bd>
2023-05-25 08:10:18 +0600

    model done

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+async function main() {
+  // code
+  const user = await prisma.user.create({ data: { name: "Abdullah" } })
+  console.log(user)
+}
+
+main()
+  .catch((e) => console.error(e.message))
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
